1cbcfa6faf5fefab3a173e259d54cfad262c703c
Tim Triche, Jr <tim.triche@gmail.com>
2011-01-02 22:26:02 -0800

    not bothering with Rcpp or MPFR due to convergence

--- src/rgammagamma_1f1.cc
+++
@@ -1,7 +0,0 @@
-#include "rgammagamma_1f1.h"
-
-using namespace Rcpp;
-
-SEXP rgammagamma_1f1( SEXP a, SEXP b, SEXP z ) {
-   
-}
--- src/rgammagamma_1f1.h
+++
@@ -1,8 +0,0 @@
-#ifndef _rgammagamma_1f1_h
-#define _rgammagamma_1f1_h
-
-#include <Rcpp.h>
-
-RcppExport SEXP rgammagamma_1f1(SEXP a, SEXP b, SEXP z);
-
-#endif
