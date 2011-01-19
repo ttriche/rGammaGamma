d6b664bca169176814e8ceb42e1ecb7dbb84d6e0
Tim Triche, Jr <tim.triche@gmail.com>
2011-01-19 13:55:20 -0800

    fixes to betamix

--- R/betamix.R
+++ R/betamix.R
@@ -30,7 +30,7 @@ beta.mode <- function(a, b) { # {{{
 
 ## beta mixture model for a "more empirical" prior on signal/noise guesses
 ## 
-beta.unmix <- function(x, niter=25, parallel=FALSE, tol=0.000001) { # {{{ 
+beta.unmix <- function(x, niter=100, rows=F, parallel=F, tol=0.01) { # {{{ 
 
   if(is(x, 'MethyLumiSet') || is(x, 'MethyLumiM')) { # {{{
     x <- pmax(1, methylated(x)) / pmax(2, (methylated(x)+unmethylated(x)))
@@ -40,21 +40,23 @@ beta.unmix <- function(x, niter=25, parallel=FALSE, tol=0.000001) { # {{{
   if(is(x, 'matrix')) { # {{{
     if(parallel) {
       require(multicore) ## FIXME: OpenMP would be better
-      data.matrix(as.data.frame(mclapply(1:dim(x)[2], function(i) {
-        beta.unmix(x[,i])
-      })))
+      if(rows) {
+        t(data.matrix(as.data.frame(mclapply(1:dim(x)[1], function(i) {
+          beta.unmix(x[i,])
+        }))))
+      } else {
+        data.matrix(as.data.frame(mclapply(1:dim(x)[2], function(i) {
+          beta.unmix(x[i,])
+        })))
+      }
     } else {
-      apply(x, 2, beta.unmix)
+      if(rows) apply(x, 1, beta.unmix)
+      else apply(x, 2, beta.unmix)
     }
   } # }}}
 
-  pi0 <- x
+  pi0 <- pmax(0.01, pmin(0.99, x))
   theta0 <- list(U=beta.mme(x, 1-pi0), M=beta.mme(x, pi0))
-  modes0 <- lapply(theta0, beta.mode)
-
-  ## a cheap hack to speed convergence
-  pi0[ which(pi0 < modes0$U) ] <- min(pi0)
-  pi0[ which(pi0 > modes0$M) ] <- max(pi0)
 
   ## FIXME: do in C++
   negloglik0 <- 10000
@@ -65,14 +67,17 @@ beta.unmix <- function(x, niter=25, parallel=FALSE, tol=0.000001) { # {{{
     negloglik1 <- -1 * sum(log(((1-pi1)*dbeta(x, theta1$U[1], theta1$U[2])) +
                                (pi1*dbeta(x, theta1$M[1], theta1$M[2])))) 
     if( abs(negloglik1 - negloglik0) < tol ) {
-      return(list(pi1=pi1, theta0=theta0, theta1=theta1))
+      return( pi1 )
     } else { 
+      discrep <- negloglik1-negloglik0
       negloglik0 <- negloglik1
       theta0 <- theta1 
       pi0 <- pi1
     }
   }
-  if(i == niter) warning("beta.unmix: Failure to converge!")
+  if(i == niter) {
+    warning("Failed convergence: discrepancy ", discrep, " at tolerance ",tol)
+  }
   return( pi1 )
 
 } # }}}
