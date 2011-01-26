83c8881c3ccd2dda0cc83e70aadff94eb99f071e
Tim Triche, Jr <tim.triche@gmail.com>
2011-01-26 15:48:55 -0800

    re-adding Rcpp hooks for mixture EM

---
+++ src/rgammagamma_beta_EM.cc
@@ -0,0 +1,11 @@
+#include "rgammagamma_1f1.h"
+
+using namespace Rcpp;
+
+SEXP rgammagamma_beta_EM( SEXP xs, SEXP pi0s, SEXP k ) {
+  
+  Rcpp::NumericVector x(xs) ;
+  Rcpp::NumericVector pi0(pi0s) ;
+
+
+}
