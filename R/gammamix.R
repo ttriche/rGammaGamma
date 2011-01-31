cc9ce2f0342ec21ecb24bfa78f8150f806ca3992
Tim Triche, Jr <tim.triche@gmail.com>
2011-01-30 23:03:25 -0800

    minor fixes to do with normexp and gamma estimates

--- R/gammamix.R
+++ R/gammamix.R
@@ -43,6 +43,15 @@ gamma.bg <- function(object, channel=NULL, channels=c('Cy3','Cy5')) { # {{{
   return(apply(negctls(object, channel), 2, function(z) gamma.mle(z)))
 } # }}}
 
+gamma.bg.ebayes <- function(object, channel=NULL, channels=c('Cy3','Cy5')){ #{{{
+  
+  # pool the gamma parameter distributions to estimate hyperparameters 
+  
+  # integrate(marginalScale, 0, Inf)
+  # integrate(marginalShape, 0, Inf)
+
+} # }}}
+
 ## FIXME: don't forget to bgcorrect the non-negative control probes too!!!
 ##
 allelic <- function(x,channel=NULL,allele=NULL,mixture=F,hard=F,parallel=F){#{{{
@@ -57,18 +66,15 @@ allelic <- function(x,channel=NULL,allele=NULL,mixture=F,hard=F,parallel=F){#{{{
   } # }}}
 
   # if we fit the fuzzy mixture, add a pi1 attribute
-  # 1-28-2011 don't do it -- doesn't work rite for mixtures
   pi0 <- methylated(x)[probes,] / total.intensity(x)[probes,] # i.e., Beta
 
   ## FIXME: should we weight pi0 by detection here?
-  # if(mixture) pi0 <- beta_unmix(x, parallel=parallel) # post-EM assignments
-  if(hard) piM <- round(pi0) # hard == dichotomized 
-  else piM <- pi0
-  piU <- 1-piM
+  if(hard) pi0 <- round(pi0) # hard == dichotomized 
+  if(mixture) pi0 <- beta_unmix(x, parallel=parallel) # post-EM assignments
 
   intensities <-list(
-    signal=((piM*methylated(x)[probes,]) + (piU*unmethylated(x)[probes,])),
-     noise=((piU*methylated(x)[probes,]) + (piM*unmethylated(x)[probes,]))
+    signal=((pi0*methylated(x)[probes,]) + ((1-pi0)*unmethylated(x)[probes,])),
+     noise=(((1-pi0)*methylated(x)[probes,]) + (pi0*unmethylated(x)[probes,]))
   )
   
   if(is.null(allele)) return(intensities) 
@@ -95,11 +101,12 @@ gamma.allelic <- gamma.fg <- function(object, channel=NULL, allele=NULL, channel
   } # }}}
 
   # unless using hard assignments, we'll just set pi0(y) to beta(y)
-  apply(allelic(object,channel=channel,allele=allele,hard=hard), 2, gamma.mle)
+  apply( c(allelic(object,channel=channel,allele=allele,hard=hard),
+           nonnegs(object,channel=channel)), 2, gamma.mle )
 
 } # }}}
 
-gamma.conditional <- function(total, params, offset=15) { # {{{
+gamma.conditional <- function(total, params) { # {{{
 
   if(length(total) > 1) sapply(total, gamma.conditional, params=params)
   if(total > (params[3]*params[4])+(10*sqrt(params[3])*params[4])){ # mu+sd.bg
@@ -118,7 +125,7 @@ gamma.conditional <- function(total, params, offset=15) { # {{{
           # print(paste('f(',x,')=',num,'/',den,'=',num/den)) # = num*(1/den)*x
         }, 
         0, total
-      )$value + offset
+      )$value # else will return a list with value, abs.error, subdivisions, ...
     )
     # integrate(PrSignalGivenTotal, 0, total)
   }
@@ -128,7 +135,6 @@ gamma.conditional <- function(total, params, offset=15) { # {{{
 ## FIXME: add a qa step for the remapped beta-mixture scheme
 ## 
 gamma.signal <- function(object, channel=NULL, allele=NULL, channels=c('Cy3','Cy5'), alleles=c('signal','noise'), hard=F, parallel=F) { # {{{
-
 
   if(is.null(channel)) { # {{{
     perchannel <- lapply(channels, function(channel) {
@@ -154,7 +160,6 @@ gamma.signal <- function(object, channel=NULL, allele=NULL, channels=c('Cy3','Cy
 
   ## should parallelize here 
   signal <- sapply(1:dim(totals)[2], function(j) {
-    # I_S_jk = gamma.conditional(I_jk, params[j,])
     sapply(totals[-pars,j], gamma.conditional, params=totals[pars,j])
   })
 
@@ -206,166 +211,6 @@ spcor.plot <- function(x, ID=NULL, parallel=TRUE) { # {{{
     lines( density(na.omit(spcs[[levelses[l]]])), 
            col=(l%%nlevels(ID))+1, lty=2 )
   }
-} # }}}
-
-# gammagamma deconvolution (my code) for comparison
-mcsv.gammagamma <- function(object,channel=NULL,allele=NULL,channels=c('Cy3','Cy5'),alleles=c('signal','noise'),annotation=NULL,betacut=0.5,robust=F, use.score=T, parallel=F, offset=25){ #{{{
-
-  x <- clone(object)
-  methylated(x) <- pmax(methylated(x), 1)
-  unmethylated(x) <- pmax(unmethylated(x), 1)
-  betas(x) <- methylated(x) / (methylated(x) + unmethylated(x))
-  if(is(x, 'MethyLumiM')) qcdata <- controlData(x)
-  if(is(x, 'MethyLumiSet')) qcdata <- QCdata(x)
-  if( is.null(QCdata(x)) ) {
-    stop('Your object does not appear to have any control bead data! Exiting.')
-  } else {
-    history.submitted <- as.character(Sys.time())
-  }
-
-  # this can and should be a methylumi or Lumi method
-  if(is.null(annotation)) annotation <- annotation(x)
-  if(is.null(annotation)) annotation <- 'IlluminaHumanMethylation27k'
-  require(paste(annotation,'db',sep='.'), character.only=T)
-  probes <- list(Cy5=cy5(x), Cy3=cy3(x))
-
-  if(ncol(betas(x)) != ncol(negctls(x, 'Cy3'))) {
-    stop("Control beads do not match the number of samples in the dataset!!!")
-  } else {
-    cat("Background mean, SD estimated from", nrow(negctls(x,'Cy3')),"probes\n")
-  }
-
-  # should use correlation structure here, or at least merge the probes;
-  # when we regress m.u.corr ~ gc.content (weighted by bisulfite conversion), 
-  # it explains a massive amount of the differences observed...
-  #
-  # for allelic deconvolution, this means ( M<0.5, U>0.5 ) and ( M>0.5, U<0.5 )
-  # works fine with beta.logit(betas(object))>0 or exprs(object)>logit(betacut)
-  #
-  alleled <- allelic(x, betacut)
-  allelic.chan <- lapply(channels, function(ch) {
-    by.allele <- lapply(alleles, function(a) {
-      list(tot=alleled[[a]][probes[[ch]],],bg=negctls(x,ch),fg=nonnegs(x,ch))
-    })
-    names(by.allele) <- alleles
-    return(by.allele)
-  })
-  names(allelic.chan) <- channels
-  if(parallel) {
-    require(multicore)
-    params <- lapply(allelic.chan, function(ch) {
-      mclapply(ch, function(al.ch) {
-        gamma.normal.params(
-          al.ch[['tot']],al.ch[['bg']],robust=robust,use.score=use.score
-        )
-      })
-    })
-  } else { 
-    params <- lapply(allelic.chan, function(ch) {
-      lapply(ch, function(al.ch) {
-        gamma.normal.params(
-          al.ch[['tot']],al.ch[['bg']],robust=robust,use.score=use.score
-        )
-      })
-    })
-
-  }
-  return(params)
-
-  #############################################################################
-  ## THIS IS AS FAR AS I HAVE GOTTEN IN TERMS OF ADAPTING NORMEXP TO NORMGAM ##
-  #############################################################################
-
-  m.u.chan <- F # mapping: merged probes to unmerged probes, sample X channel 
-
-  zz <- zmu <- zsigma <- zalpha <- list()
-  for( channel in c('cy3','cy5') ) {
-    dat <- rbind(dat.cpg[[channel]][['m']], dat.cpg[[channel]][['u']])
-    m <- nrow(dat) 
-    n <- ncol(dat)
-    mu <- sigma <- alpha <- rep(NA, n)
-    z <- rbind(dat, dat.pctrl[[channel]], dat.nctrl[[channel]])
-    for(i in 1:n){
-      #if(robust){ 
-
-      #} else { 
-        mu[i] <- mean(dat.nctrl[[channel]][, i])
-        sigma[i] <- sd(dat.nctrl[[channel]][, i])
-        alpha[i] <- max(mean(dat[, i]) - mu[i], 10)
-      #}
-      z[, i] <- mcsv.nexp.signal(c(mu[i], log(sigma[i]), log(alpha[i])), z[,i])
-    }
-    z <- z + offset
-    zz[[channel]] <- z
-    zmu[[channel]] <- mu
-    zsigma[[channel]] <- sigma
-    zalpha[[channel]] <- alpha
-  }
-
-  # jointly estimated
-  m.x <- methylated(x)
-  u.x <- unmethylated(x)
-  for( channel in c('cy3','cy5') ) {
-    m.x[ probes[[channel]], ] <- zz[[channel]][mprobes[[channel]], ]
-    u.x[ probes[[channel]], ] <- zz[[channel]][uprobes[[channel]], ]
-    dat.ctrl[[channel]][pos, ] <- zz[[channel]][pcprobes[[channel]], ]
-    dat.ctrl[[channel]][neg, ] <- zz[[channel]][ncprobes[[channel]], ]
-  }
-  methylated(x) <- m.x
-  unmethylated(x) <- u.x
-  dat.nctrl <- list(cy5=Cy5(ctrl)[neg,], cy3=Cy3(ctrl)[neg,])
-  dat.pctrl <- list(cy5=Cy5(ctrl)[pos,], cy3=Cy3(ctrl)[pos,])
-  pData(x) <- cbind(pData(x), mu.Cy5=zmu$cy5, mu.Cy3=zmu$cy3, 
-                              sigma.Cy5=zsigma$cy5, sigma.Cy3=zsigma$cy3,
-                              alpha.Cy5=zalpha$cy5, alpha.Cy3=zalpha$cy3)
-  Cy3(ctrl) <- dat.ctrl$cy3
-  Cy5(ctrl) <- dat.ctrl$cy5
-  QCdata(x) <- ctrl # adjusted 
-
-	varMetadata(x)[rownames(varMetadata(x)) == "mu.Cy5", ] <- 
-    "Estimated mean background intensity, red channel"
-	varMetadata(x)[rownames(varMetadata(x)) == "mu.Cy3", ] <- 
-    "Estimated mean background intensity, green channel"
-	varMetadata(x)[rownames(varMetadata(x)) == "sigma.Cy5", ] <- 
-    "Estimated standard deviation of background intensities, red channel"
-	varMetadata(x)[rownames(varMetadata(x)) == "sigma.Cy3", ] <- 
-    "Estimated standard deviation of background intensities, green channel"
-	varMetadata(x)[rownames(varMetadata(x)) == "alpha.Cy5", ] <- 
-    "Estimated mean signal intensity, red channel"
-	varMetadata(x)[rownames(varMetadata(x)) == "alpha.Cy3", ] <- 
-    "Estimated mean signal intensity, green channel"
-
-  history.command <- "Applied normexp background correction."
-  history.finished <- t.finish()
-  x@history<- rbind(x@history,
-                    data.frame(submitted=history.submitted,
-                               finished=history.finished,
-                               command=history.command))
-  return(x)
-  zz <- zmu <- zsigma <- zalpha <- list()
-  for( channel in c('cy3','cy5') ) {
-    dat <- rbind(dat.cpg[[channel]][['m']], dat.cpg[[channel]][['u']])
-    m <- nrow(dat) 
-    n <- ncol(dat)
-    mu <- sigma <- alpha <- rep(NA, n)
-    z <- rbind(dat, dat.pctrl[[channel]], dat.nctrl[[channel]])
-    for(i in 1:n){
-      #if(robust){ 
-
-      #} else { 
-        mu[i] <- mean(dat.nctrl[[channel]][, i])
-        sigma[i] <- sd(dat.nctrl[[channel]][, i])
-        alpha[i] <- max(mean(dat[, i]) - mu[i], 10)
-      #}
-      z[, i] <- mcsv.nexp.signal(c(mu[i], log(sigma[i]), log(alpha[i])), z[,i])
-    }
-    z <- z + offset
-    zz[[channel]] <- z
-    zmu[[channel]] <- mu
-    zsigma[[channel]] <- sigma
-    zalpha[[channel]] <- alpha
-  }
-
 } # }}}
 
 mcsv.nexp.signal <- function (par, x)  { # {{{
